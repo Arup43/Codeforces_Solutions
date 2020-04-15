4e446e64f38df57529ac2dac8ec15d50dbda950e
Arup43 <61915814+Arup43@users.noreply.github.com>
2020-04-16 01:04:35 +0600

    Update 1337B-Kana-and-Dragon-Quest-game.cpp

--- 1337B-Kana-and-Dragon-Quest-game.cpp
+++ 1337B-Kana-and-Dragon-Quest-game.cpp
@@ -1,1 +1,27 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T,x,n,m;
+    cin>>T;
+    for(int i=0;i<T;i++)
+    {
+        cin>> x>>n>>m;
+        while(x>20 && n>0)
+        {
+            x=x/2+10;
+            n--;
+        }
+        x=x-m*10;
+        if(x<=0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
